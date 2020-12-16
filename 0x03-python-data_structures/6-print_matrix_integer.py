7c89f16e9af5c1d52edb17fede868650677a78f2
CarolinaHV <carolina.hv02@gmail.com>
2020-12-16 05:41:33 +0000

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    cont = 0
     for row in matrix:
         esp = ''
         for column in row:
-            if cont == len(row):
-                print("{:s}{:d}".format(esp, column), end="")
-            else:
-                print("{:s}{:d}".format(esp, column), end="")
-            cont += 1
-        esp = ' '
-        print(' ')
+            print("{:s}{:d}".format(esp, column), end="")
+            esp = ' '
+        print('')
