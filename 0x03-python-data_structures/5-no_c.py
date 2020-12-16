19d9b055f0db709881582ed21d71f322c01625d7
CarolinaHV <carolina.hv02@gmail.com>
2020-12-16 00:09:37 +0000

    Remove letter of a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    w_list = list(my_string)
+    for char in w_list:
+        if char == 'C':
+            w_list.remove('C')
+        elif char == 'c':
+            w_list.remove('c')
+        new_list = ''.join(w_list)
+    return(new_list)
