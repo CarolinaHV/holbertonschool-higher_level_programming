3a85d6a36738842d7107b863e7a33c88b995572a
CarolinaHV <carolina.hv02@gmail.com>
2021-01-04 21:43:07 +0000

    Exception IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num_print = 0
-    for element in range (x):
+    for element in range(x):
         try:
             print(my_list[element], end='')
             num_print += 1
