4da9af3dde6ce2c656db66605db9d0ca38351d84
CarolinaHV <carolina.hv02@gmail.com>
2021-01-06 19:18:42 +0000

    Coordinates of a square.4

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,7 @@ class Square():
     """Instantiation of a private attribute"""
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
-        self.position = position
+        self.__position = position
 
     """Getter size"""
     @property
@@ -31,9 +31,11 @@ class Square():
     @position.setter
     def position(self, value):
         self.__position = value
-        if(type(value)) is not tuple or not int or len(value) != 2:
+        if(type(value)) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     """Area of a square"""
