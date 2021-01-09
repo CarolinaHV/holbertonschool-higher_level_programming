f05c6c4022c2458bae460ed0cc6a15b123d1b03d
CarolinaHV <carolina.hv02@gmail.com>
2021-01-09 04:49:19 +0000

    Remove file

--- 0-square_matrix_simple.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for i in range(len(matrix)):
-        new_matrix.append(list(map(lambda i: i * i, matrix[i])))
-    return new_matrix
