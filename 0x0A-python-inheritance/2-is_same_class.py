c960f4ac8fe60888119b30999c93f43ff8682e7f
CarolinaHV <carolina.hv02@gmail.com>
2021-01-20 01:32:18 +0000

    object is an instance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This function returns True
+if the object is exactly an instance
+of the specified class otherwise
+return False
+
+"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
