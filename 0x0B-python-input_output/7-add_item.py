391ebad7799b00c3b15e67f13501773d1d093014
CarolinaHV <carolina.hv02@gmail.com>
2021-01-21 05:00:44 +0000

    INPUT/OUTPUT/JSON

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,16 @@ and then save them to a file
 
 import sys
 
-save_to_json_f = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_f = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_f = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_f = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
+
+try:
+    obj = load_from_json_f(filename)
+except:
+    obj = []
+
+for x in sys.argv[1:]:
+    obj.append(x)
+save_to_json_f(obj, filename)
