8eb83f941e8b5a02463b4e7428888e7b88beebbe
CarolinaHV <carolina.hv02@gmail.com>
2021-01-21 04:26:13 +0000

    INPUT/OUTPUT/JSON

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,6 @@ and returns the number of characters written
 
 
 def write_file(filename="", text=""):
+    """Write file"""
     with open(filename, 'w') as holb_text:
         return holb_text.write(text)
