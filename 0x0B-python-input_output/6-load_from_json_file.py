d0835353654ab924ce3ecdddcf93647e61cb2ce0
CarolinaHV <carolina.hv02@gmail.com>
2021-01-21 04:03:08 +0000

    INPUT/OUTPUT/JSON

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function creates an Object from a “JSON file”
+
+"""
+import json
+
+
+def load_from_json_file(filename):
+    with open(filename, encoding="utf-8") as holb_file:
+        return (json.load(holb_file))
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+script that adds all arguments to a Python list
+and then save them to a file
+
+"""
+
+import sys
+
+save_to_json_f = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_f = __import__('6-load_from_json_file.py').load_from_json_file
+
