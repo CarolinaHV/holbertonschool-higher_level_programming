a32f1acceb40ddf40fdcbea4c1f34b13e9c51341
CarolinaHV <carolina.hv02@gmail.com>
2021-01-21 04:39:03 +0000

    INPUT/OUTPUT/JSON

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,6 @@ import json
 
 
 def load_from_json_file(filename):
+    """Create object from a JSON file"""
     with open(filename, encoding="utf-8") as holb_file:
         return (json.load(holb_file))
