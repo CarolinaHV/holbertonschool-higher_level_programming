92235f3a5a3c5c70369b04fba299c7bc41c0c1fc
CarolinaHV <carolina.hv02@gmail.com>
2021-03-02 04:38:01 +0000

    Add and listed by descending score in the table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- This script lists all records of the table second_table
 -- of the database hbtn_0c_0 in MySQL server.
-SELECT score, name FROM second_table WHERE name IS NOT NULL AND score >= 10
+SELECT score, name FROM second_table WHERE name IS NOT NULL
        ORDER BY score DESC, name;
