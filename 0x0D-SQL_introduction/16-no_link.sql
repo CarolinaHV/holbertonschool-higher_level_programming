5ca114a54346c082806b00dd991b6879ea0c2709
CarolinaHV <carolina.hv02@gmail.com>
2021-03-02 04:22:15 +0000

    Add and listed by descending score in the table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,5 +2,5 @@
 -- of the database hbtn_0c_0 in MySQL server.
 INSERT INTO `second_table` (`score`, `name`) VALUES ("12", "Aria");
 INSERT INTO `second_table` (`score`, `name`) VALUES ("18", "Aria");
-SELECT score, name FROM second_table WHERE score >= 10
+SELECT score, name FROM second_table WHERE IS NOT NULL score >= 10
        ORDER BY score DESC, name;
