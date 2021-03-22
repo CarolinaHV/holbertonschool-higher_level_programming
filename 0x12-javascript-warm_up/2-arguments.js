e45dca4948cf82870956570a182e14c93f8993d9
CarolinaHV <carolina.hv02@gmail.com>
2021-03-22 07:25:21 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* This script prints a message depending
+of the number of arguments passed */
+const myArgs = process.argv.slice(2);
+if (myArgs.length === 0) {
+  console.log('No arguments');
+} else if (myArgs.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
