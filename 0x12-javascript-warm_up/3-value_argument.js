b2e124ffcc7cedb9e80aff01488bc0158a69a4f7
CarolinaHV <carolina.hv02@gmail.com>
2021-03-23 00:42:28 +0000

    Arguments / fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-/* This script prints the first argument passed to it */
+/*
+   This script prints the first argument passed to it
+*/
 const myArgs = process.argv.slice(2);
 if (myArgs[0] === undefined) {
-  console.log('No arguments');
+  console.log('No argument');
 } else {
   console.log(`${myArgs}`);
 }
