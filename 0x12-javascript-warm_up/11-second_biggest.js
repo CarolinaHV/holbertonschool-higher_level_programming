30bb7f701ab8fd1039a2f39891cf4b8ac5632bd6
CarolinaHV <carolina.hv02@gmail.com>
2021-03-23 04:34:36 +0000

    Second biggest / fix with lambda

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -10,7 +10,6 @@ const num = (process.argv.slice(2));
 if (num.length < 2) {
   console.log(0);
 } else {
-    num.sort((a, b) => a - b);
-    const compare = Math.max(num[num.length - 2]);
-  console.log(compare);
+  num.sort((a, b) => a - b);
+  console.log(parseInt(num[num.length - 2]));
 }
