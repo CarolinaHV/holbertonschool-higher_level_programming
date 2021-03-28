e55a59588f8a3fdeaadce655427f9c3a530c84b9
CarolinaHV <carolina.hv02@gmail.com>
2021-03-28 17:13:24 +0000

    Filter states by user input/FIX PEP8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     cur.execute("""SELECT * FROM states
     WHERE name LIKE '{}'
     ORDER BY id ASC""".format(argv[4]))
-    
+
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
