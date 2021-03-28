dcdd08a37ca0ce9ff09e055fb02e9c421e63fc00
CarolinaHV <carolina.hv02@gmail.com>
2021-03-28 17:55:24 +0000

    All cities by state/FIXED

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''
-This script that lists all states with a name starting
-with N (upper N) from the database h.
+This script that lists the cities from the database hbtn_0e_4_usa
 
 '''
 
@@ -15,17 +14,17 @@ if __name__ == "__main__":
                            port=3306,
                            user=argv[1],
                            passwd=argv[2],
-                           db=argv[3],
-                           charset="utf-8")
+                           db=argv[3])
 
     ''' Create a cursor '''
     cur = conn.cursor()
 
-    ''' Query the database - list all the rows '''
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM states 
-    JOIN cities ON  cities.states_id = states.id  
+    ''' Query the database '''
+    cur.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id=states.id
     ORDER BY cities.id ASC""")
-    
+
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
