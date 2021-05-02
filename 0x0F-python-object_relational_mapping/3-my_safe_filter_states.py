33c5c6807b4abbe5db775f8cc47964094e805a61
CarolinaHV <carolina.hv02@gmail.com>
2021-05-02 19:26:21 +0000

    PYTHON - ORM

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+ SQL Injection...
+
+'''
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    ''' Connect to database '''
+    conn = MySQLdb.connect(host='localhost',
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+
+    ''' Create a cursor '''
+    cur = conn.cursor()
+    
+    ''' Query the database - list all the rows '''
+    cur.execute("""SELECT * FROM `states`
+    WHERE `name`=(%s) ORDER BY `id` ASC""", (argv[4],))
+   
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+This script takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa
+
+'''
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    ''' Connect to database '''
+    conn = MySQLdb.connect(host='localhost',
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+
+    ''' Create a cursor '''
+    cur = conn.cursor()
+
+    ''' Query the database '''
+    cur.execute("""SELECT `cities.name`
+    FROM `cities`
+    JOIN `states` ON cities.state_id=states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC""", (argv[4], )
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
