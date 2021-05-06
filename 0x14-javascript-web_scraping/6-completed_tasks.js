3658c58d8be29e876b53a21c466bf99775f3c650
CarolinaHV <carolina.hv02@gmail.com>
2021-05-06 01:50:58 +0000

    Web scraping-change

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,14 +8,14 @@ request(url, function (err, data, body) {
   if (err) {
     console.log(err);
   } else {
-    let response = JSON.parse(body);
+    const response = JSON.parse(body);
 
     for (let i = 0; i < response.length; i++) {
-      if (response[i]['completed'] === true) {
-        if (empDict[response[i]['userId']] === undefined) {
-          empDict[response[i]['userId']] = 1;
+      if (response[i].completed === true) {
+        if (empDict[response[i].userId] === undefined) {
+          empDict[response[i].userId] = 1;
         } else {
-          empDict[response[i]['userId']] += 1;
+          empDict[response[i].userId] += 1;
         }
       }
     }
