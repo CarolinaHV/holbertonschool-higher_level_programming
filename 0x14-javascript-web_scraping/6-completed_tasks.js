f285716b8054fc9231eba60a8c9a916a4b7ceefb
CarolinaHV <carolina.hv02@gmail.com>
2021-05-06 01:46:31 +0000

    Web scraping-change

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,29 +2,23 @@
 
 const request = require('request');
 const url = process.argv[2];
-const emp = {};
-request(url, function (err, response, body) {
-  if (err) {
-    console.error(err);
-  } else {
-    const info = JSON.parse(body);
-    let i = 0;
-    while (info[i]) {
-      const userId = info[i].userId;
-      let count = 0;
-      let j = i;
+const empDict = {};
 
-      while (info[j] && info[j].userId === userId) {
-        if (info[j].completed) {
-          count++;
+request(url, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let response = JSON.parse(body);
+
+    for (let i = 0; i < response.length; i++) {
+      if (response[i]['completed'] === true) {
+        if (empDict[response[i]['userId']] === undefined) {
+          empDict[response[i]['userId']] = 1;
+        } else {
+          empDict[response[i]['userId']] += 1;
         }
-        j++;
       }
-      if (count !== 0) {
-        emp[userId] = count;
-      }
-      i = j;
     }
-    console.log(emp);
   }
+  console.log(empDict);
 });
