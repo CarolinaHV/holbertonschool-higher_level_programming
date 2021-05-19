2ab9e04096ece91859e91c9a8b8e99c38edfbf7c
CarolinaHV <carolina.hv02@gmail.com>
2021-05-19 00:03:10 +0000

    Task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+$.ajax({
+  url: 'https://swapi.co/api/films/?format=json',
+  success: function (result) {
+    result.results.map(function (item) {
+      $('UL#list_movies').append('<li>' + item.title + '</li>');
+    });
+  }
+});
