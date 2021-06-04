ec7f19bdf608ef3e0603c692a88e068054759894
CarolinaHV <carolina.hv02@gmail.com>
2021-06-04 20:26:38 +0000

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+
+"""
+
+
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    req = Request(url)
+
+    try:
+        with urlopen(url) as x:
+            print(x.read().decode('utf-8'))
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))
