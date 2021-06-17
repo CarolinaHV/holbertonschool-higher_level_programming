4b8dbe4938abca6eb14c49b597db9a74015b8f6a
CarolinaHV <carolina.hv02@gmail.com>
2021-06-17 07:14:20 +0000

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,13 +15,14 @@ if __name__ == "__main__":
                            port=3306,
                            user=argv[1],
                            passwd=argv[2],
-                           db=argv[3])
+                           db=argv[3],
+                           charset='UTF8')
 
     ''' Create a cursor '''
     cur = conn.cursor()
 
     ''' Query the database - list all the rows '''
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BYNARY 'N%' ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
